1b0063394021f74a745f99e0925af6c94786dae1
Luis Zanabria - ADSI <lazanabria2@misena.edu.co>
2022-10-12 12:15:52 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let nums = 0;
+    let pago_sum = 0, desc = 10, val_desc, total;
     addEventListener("submit", (e) => {
         e.preventDefault();
-        if (e.submitter.dataset.operacion != "REINICIAR") {
+        if (e.submitter.dataset.operacion == "GUARDAR") {
             let data = Object.fromEntries(new FormData(e.target));
-            if (data.numero > 0) {
-                nums += Number(data.numero);
+            if (data.montos > 0) {
+                pago_sum += Number(data.montos);
                 e.target.reset();
             }
-            else {
+            else if (data.montos == 0) {
                 e.submitter.disabled = true;
-                document.querySelector('[id="res"]').value = nums;
+                document.querySelector('#sub').value = "Monto de las compras: $" + pago_sum;
+                if (pago_sum > 10000) {
+                    val_desc = pago_sum * desc / 100;
+                    total = pago_sum - val_desc;
+                    document.querySelector('#desc').value = "Descuento: " + desc + "%";
+                    document.querySelector('#val-desc').value = "Valor del descuento: $" + val_desc;
+                    document.querySelector('#total').value = "Total a pagar: $" + total;
+                }
+                else {
+                    document.querySelector('#desc').value = "Descuento: No aplica";
+                    document.querySelector('#val-desc').value = "Valor del descuento: No aplica";
+                    document.querySelector('#total').value = "Total a pagar: $" + pago_sum;
+                }
             }
-        } 
+            else if (data.montos < 0) {
+                alert("¡Monto negativo!\nIngrese un monto válido nuevamente.");
+                e.target.reset();
+            }
+        }
+
         else {
             location.reload();
         }
